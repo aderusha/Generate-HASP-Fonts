851e250af6109afcadcc1b6553142f4e02a89050
aderusha <aderusha@users.noreply.github.com>
2020-03-22 19:18:08 -0400

    Add icon size offset

--- Generate-HASP-Fonts.ps1
+++ Generate-HASP-Fonts.ps1
@@ -15,6 +15,7 @@
 #
 # MaterialDesign font included here sourced from:
 # https://github.com/Templarian/MaterialDesign-Font
+# License: https://github.com/Templarian/MaterialDesign/blob/master/LICENSE
 #
 ####################################################################################################
 #
@@ -46,57 +47,63 @@ Function Main {
   $textFont = "./NotoSans-Regular.ttf" | Get-ChildItem | ForEach-Object { $_.FullName }
   $iconFont = "./MaterialDesignIconsDesktop.ttf" | Get-ChildItem | ForEach-Object { $_.FullName }
 
-  $fontSize = 24
+  $textFontSize = 24
   $textVerticalOffset = 0
+  $iconFontSizeOffset = 2
+  $iconVerticalOffset = 6
+  New-ZiFontV5 -textFont $textFont -iconFont $iconFont -iconFontFirstCP 0xf0001 -iconFontLastCP 0xf13fe -iconCPOffset 0xE2001 -iconVerticalOffset $iconVerticalOffset -textVerticalOffset $textVerticalOffset -textFontSize $textFontSize -iconFontSizeOffset $iconFontSizeOffset -Codepage $codePage -Path $outfile
+
+  $textFontSize = 32
+  $textVerticalOffset = 0
+  $iconFontSizeOffset = 4
   $iconVerticalOffset = 4
-  New-ZiFontV5 -textFont $textFont -iconFont $iconFont -iconFontFirstCP 0xf0001 -iconFontLastCP 0xf13fe -iconCPOffset 0xE2001 -iconVerticalOffset $iconVerticalOffset -textVerticalOffset $textVerticalOffset -size $fontSize -Codepage $codePage -Path $outfile
+  New-ZiFontV5 -textFont $textFont -iconFont $iconFont -iconFontFirstCP 0xf0001 -iconFontLastCP 0xf13fe -iconCPOffset 0xE2001 -iconVerticalOffset $iconVerticalOffset -textVerticalOffset $textVerticalOffset -textFontSize $textFontSize -iconFontSizeOffset $iconFontSizeOffset -Codepage $codePage -Path $outfile
 
-  $fontSize = 32
+  $textFontSize = 48
   $textVerticalOffset = 0
+  $iconFontSizeOffset = 8
   $iconVerticalOffset = 4
-  New-ZiFontV5 -textFont $textFont -iconFont $iconFont -iconFontFirstCP 0xf0001 -iconFontLastCP 0xf13fe -iconCPOffset 0xE2001 -iconVerticalOffset $iconVerticalOffset -textVerticalOffset $textVerticalOffset -size $fontSize -Codepage $codePage -Path $outfile
+  New-ZiFontV5 -textFont $textFont -iconFont $iconFont -iconFontFirstCP 0xf0001 -iconFontLastCP 0xf13fe -iconCPOffset 0xE2001 -iconVerticalOffset $iconVerticalOffset -textVerticalOffset $textVerticalOffset -textFontSize $textFontSize -iconFontSizeOffset $iconFontSizeOffset -Codepage $codePage -Path $outfile
 
-  $fontSize = 48
+  $textFontSize = 64
   $textVerticalOffset = 0
-  $iconVerticalOffset = 8
-  New-ZiFontV5 -textFont $textFont -iconFont $iconFont -iconFontFirstCP 0xf0001 -iconFontLastCP 0xf13fe -iconCPOffset 0xE2001 -iconVerticalOffset $iconVerticalOffset -textVerticalOffset $textVerticalOffset -size $fontSize -Codepage $codePage -Path $outfile
+  $iconFontSizeOffset = 8
+  $iconVerticalOffset = 4
+  New-ZiFontV5 -textFont $textFont -iconFont $iconFont -iconFontFirstCP 0xf0001 -iconFontLastCP 0xf13fe -iconCPOffset 0xE2001 -iconVerticalOffset $iconVerticalOffset -textVerticalOffset $textVerticalOffset -textFontSize $textFontSize -iconFontSizeOffset $iconFontSizeOffset -Codepage $codePage -Path $outfile
 
-  $fontSize = 64
-  $textVerticalOffset = 0
-  $iconVerticalOffset = 8
-  New-ZiFontV5 -textFont $textFont -iconFont $iconFont -iconFontFirstCP 0xf0001 -iconFontLastCP 0xf13fe -iconCPOffset 0xE2001 -iconVerticalOffset $iconVerticalOffset -textVerticalOffset $textVerticalOffset -size $fontSize -Codepage $codePage -Path $outfile
-
-  $fontSize = 80
+  $textFontSize = 80
   $textVerticalOffset = -12
-  $iconVerticalOffset = 4
-  New-ZiFontV5 -textFont $textFont -iconFont $iconFont -iconFontFirstCP 0xf0001 -iconFontLastCP 0xf13fe -iconCPOffset 0xE2001 -iconVerticalOffset $iconVerticalOffset -textVerticalOffset $textVerticalOffset -size $fontSize -Codepage $codePage -Path $outfile
+  $iconFontSizeOffset = 8
+  $iconVerticalOffset = 0
+  New-ZiFontV5 -textFont $textFont -iconFont $iconFont -iconFontFirstCP 0xf0001 -iconFontLastCP 0xf13fe -iconCPOffset 0xE2001 -iconVerticalOffset $iconVerticalOffset -textVerticalOffset $textVerticalOffset -textFontSize $textFontSize -iconFontSizeOffset $iconFontSizeOffset -Codepage $codePage -Path $outfile
 
   $textFont = "./NotoSans-Bold.ttf" | Get-ChildItem | ForEach-Object { $_.FullName }
-  $fontSize = 80
+  $textFontSize = 80
   $textVerticalOffset = -12
-  $iconVerticalOffset = 4
-  New-ZiFontV5 -textFont $textFont -iconFont $iconFont -iconFontFirstCP 0xf0001 -iconFontLastCP 0xf13fe -iconCPOffset 0xE2001 -iconVerticalOffset $iconVerticalOffset -textVerticalOffset $textVerticalOffset -size $fontSize -Codepage $codePage -Path $outfile
+  $iconFontSizeOffset = 8
+  $iconVerticalOffset = 0
+  New-ZiFontV5 -textFont $textFont -iconFont $iconFont -iconFontFirstCP 0xf0001 -iconFontLastCP 0xf13fe -iconCPOffset 0xE2001 -iconVerticalOffset $iconVerticalOffset -textVerticalOffset $textVerticalOffset -textFontSize $textFontSize -iconFontSizeOffset $iconFontSizeOffset -Codepage $codePage -Path $outfile
 
   $textFont = "$($Env:windir)\Fonts\Consola.ttf"
-  $fontSize = 24
+  $textFontSize = 24
   $textVerticalOffset = 0
-  New-ZiFontV5 -textFont $textFont -textVerticalOffset $textVerticalOffset -size $fontSize -Codepage $codePage -Path $outfile
+  New-ZiFontV5 -textFont $textFont -textVerticalOffset $textVerticalOffset -textFontSize $textFontSize -Codepage $codePage -Path $outfile
 
-  $fontSize = 32
-  New-ZiFontV5 -textFont $textFont -textVerticalOffset $textVerticalOffset -size $fontSize -Codepage $codePage -Path $outfile
+  $textFontSize = 32
+  New-ZiFontV5 -textFont $textFont -textVerticalOffset $textVerticalOffset -textFontSize $textFontSize -Codepage $codePage -Path $outfile
 
-  $fontSize = 48
-  New-ZiFontV5 -textFont $textFont -textVerticalOffset $textVerticalOffset -size $fontSize -Codepage $codePage -Path $outfile
+  $textFontSize = 48
+  New-ZiFontV5 -textFont $textFont -textVerticalOffset $textVerticalOffset -textFontSize $textFontSize -Codepage $codePage -Path $outfile
 
   $textFont = "$($Env:windir)\Fonts\Consolab.ttf"
-  $fontSize = 80
+  $textFontSize = 80
   $textVerticalOffset = -12
-  New-ZiFontV5 -textFont $textFont -textVerticalOffset $textVerticalOffset -size $fontSize -Codepage $codePage -Path $outfile
+  New-ZiFontV5 -textFont $textFont -textVerticalOffset $textVerticalOffset -textFontSize $textFontSize -Codepage $codePage -Path $outfile
 
   $textFont = "$($Env:windir)\Fonts\Webdings.ttf"
-  $fontSize = 56
+  $textFontSize = 56
   $textVerticalOffset = 0
-  New-ZiFontV5 -textFont $textFont -textVerticalOffset $textVerticalOffset -size $fontSize -Codepage $codePage -Path $outfile
+  New-ZiFontV5 -textFont $textFont -textVerticalOffset $textVerticalOffset -textFontSize $textFontSize -Codepage $codePage -Path $outfile
 }
 
 Function New-ZiFontV5 {
@@ -107,11 +114,12 @@ Function New-ZiFontV5 {
     [Parameter(Position = 2, Mandatory = $false, HelpMessage = "Icon font start codepoint")][int]$iconFontFirstCP,
     [Parameter(Position = 3, Mandatory = $false, HelpMessage = "Icon font end codepoint")][int]$iconFontLastCP,
     [Parameter(Position = 4, Mandatory = $false, HelpMessage = "Icon font codepoint offset to fit result below 0xFFFF")][int]$iconCPOffset = 0,
-    [Parameter(Position = 5, Mandatory = $false, HelpMessage = "Icon font vertical offset")][int]$iconVerticalOffset = 0,
-    [Parameter(Position = 6, Mandatory = $false, HelpMessage = "Text font vertical offset")][int]$textVerticalOffset = 0,
-    [Parameter(Position = 7, Mandatory = $true, HelpMessage = "Generated font size")][byte[]]$size,
-    [Parameter(Position = 8, Mandatory = $false, HelpMessage = "Font codepage")]$Codepage = [ZiLib.CodePageIdentifier]::utf_8,
-    [Parameter(Position = 9, Mandatory = $true, HelpMessage = "Generated font output folder")][string]$Path
+    [Parameter(Position = 5, Mandatory = $false, HelpMessage = "Text font vertical offset")][int]$textVerticalOffset = 0,
+    [Parameter(Position = 6, Mandatory = $false, HelpMessage = "Icon font vertical offset")][int]$iconVerticalOffset = 0,
+    [Parameter(Position = 7, Mandatory = $true, HelpMessage = "Text font size")][int]$textFontSize,
+    [Parameter(Position = 8, Mandatory = $true, HelpMessage = "Icon font size offset from text size")][int]$iconFontSizeOffset,
+    [Parameter(Position = 9, Mandatory = $false, HelpMessage = "Font codepage")]$Codepage = [ZiLib.CodePageIdentifier]::utf_8,
+    [Parameter(Position = 10, Mandatory = $true, HelpMessage = "Generated font output folder")][string]$Path
   )
 
   $iconSet = @()
@@ -121,78 +129,79 @@ Function New-ZiFontV5 {
   $locationText = [System.Drawing.PointF]::new(0, $textVerticalOffset)
   $locationIcon = [System.Drawing.PointF]::new(0, $iconVerticalOffset)
 
-  foreach ($fontsize in $size) {
+  $file = Get-ChildItem $textFont
 
-    $file = Get-ChildItem $textFont
-    $newfontsize = $fontsize
 
-    # Check if fontname is a otf/ttf file
-    if ((($file.Extension -eq ".ttf") -or ($file.Extension -eq ".otf")) -and ($file.Exists)) {
-      $pfc = [System.Drawing.Text.PrivateFontCollection]::new()
-      $pfc.AddFontFile($textFont)
-      $font = [System.Drawing.Font]::new($pfc.Families[0], $newfontsize, "regular", [System.Drawing.GraphicsUnit]::pixel );
+  # Check if fontname is a otf/ttf file
+  if ((($file.Extension -eq ".ttf") -or ($file.Extension -eq ".otf")) -and ($file.Exists)) {
+    $pfc = [System.Drawing.Text.PrivateFontCollection]::new()
+    $pfc.AddFontFile($textFont)
+    $font = [System.Drawing.Font]::new($pfc.Families[0], $textFontSize, "regular", [System.Drawing.GraphicsUnit]::pixel );
       
-      if ($iconFont) {
-        $pfc.AddFontFile($iconFont)
-        $iconFontSet = $pfc.families | Select-Object -First 1
+    if ($iconFont) {
+      $pfc.AddFontFile($iconFont)
+      $iconFontSet = $pfc.families | Select-Object -First 1
+    }
+  }
+  else {
+    $font = [ZiLib.Extensions.BitmapExtensions]::GetFont($textFont, $textFontSize, "Regular")
+  }
+
+  $newfontsize = $textFontSize
+  $i = 0
+  while ([math]::Abs($textFontSize - $font.Height) -gt 0.1 -and $i++ -lt 10) {
+    $newfontsize += ($textFontSize - $font.Height) / 2 * $newfontsize / $font.Height;
+    $font = [System.Drawing.Font]::new($font.fontfamily, $newfontsize, "regular", [System.Drawing.GraphicsUnit]::pixel );
+  }
+
+  Write-Host "Final text size: $newfontsize"
+
+  $f = [ZiLib.FileVersion.V5.ZiFontV5]::new()
+  $f.CharacterHeight = $textFontSize
+  $f.CodePage = $codepage
+  $f.Version = 5
+
+  # Letters
+  foreach ($ch in 32..255) {
+    # normal text
+    $bytes = [bitconverter]::GetBytes([uint16]$ch)
+    if ($f.CodePage.CodePageIdentifier -eq "UTF_8") {
+      if ($ch -lt 0x00d800 -or $ch -gt 0x00dfff) {
+        $txt = [Char]::ConvertFromUtf32([uint32]($ch + $codepoint.delta))
       }
+      else { $txt = "?" }
     }
     else {
-      $font = [ZiLib.Extensions.BitmapExtensions]::GetFont($textFont, $newfontsize, "Regular")
-    }
-
-    $i = 0
-    while ([math]::Abs($fontsize - $font.Height) -gt 0.1 -and $i++ -lt 10) {
-      $newfontsize += ($fontsize - $font.Height) / 2 * $newfontsize / $font.Height;
-      $font = [System.Drawing.Font]::new($font.fontfamily, $newfontsize, "regular", [System.Drawing.GraphicsUnit]::pixel );
-    }
-
-    $f = [ZiLib.FileVersion.V5.ZiFontV5]::new()
-    $f.CharacterHeight = $fontsize
-    $f.CodePage = $codepage
-    $f.Version = 5
-
-    # Letters
-    foreach ($ch in 32..255) {
-      # normal text
-      $bytes = [bitconverter]::GetBytes([uint16]$ch)
-      if ($f.CodePage.CodePageIdentifier -eq "UTF_8") {
-        if ($ch -lt 0x00d800 -or $ch -gt 0x00dfff) {
-          $txt = [Char]::ConvertFromUtf32([uint32]($ch + $codepoint.delta))
-        }
-        else { $txt = "?" }
+      if ($ch -gt 255) {
+        $txt = $f.CodePage.Encoding.GetChars($bytes, 0, 2)
       }
       else {
-        if ($ch -gt 255) {
-          $txt = $f.CodePage.Encoding.GetChars($bytes, 0, 2)
-        }
-        else {
-          $txt = $f.CodePage.Encoding.GetChars($bytes, 0, 1)
-        }
+        $txt = $f.CodePage.Encoding.GetChars($bytes, 0, 1)
       }
+    }
 
-      $character = [ZiLib.FileVersion.Common.ZiCharacter]::FromString($f, $ch, $font, $locationText, $txt)
+    $character = [ZiLib.FileVersion.Common.ZiCharacter]::FromString($f, $ch, $font, $locationText, $txt)
+    $f.AddCharacter($ch, $character)
+  }
+
+  if ($iconFont) {
+    $font = [System.Drawing.Font]::new($iconFontSet, ($newfontsize + $iconFontSizeOffset), "regular", [System.Drawing.GraphicsUnit]::pixel )
+    foreach ($ch in $iconFontFirstCP..$iconFontLastCP) {
+      $txt = [Char]::ConvertFromUtf32([uint32]($ch))
+      $character = [ZiLib.FileVersion.Common.ZiCharacter]::FromString($f, $ch - $iconCPOffset, $font, $locationIcon, $txt)
       $f.AddCharacter($ch, $character)
     }
-
-    if ($iconFont) {
-      $font = [System.Drawing.Font]::new($iconFontSet, $newfontsize, "regular", [System.Drawing.GraphicsUnit]::pixel )
-      foreach ($ch in $iconFontFirstCP..$iconFontLastCP) {
-        $txt = [Char]::ConvertFromUtf32([uint32]($ch))
-        $character = [ZiLib.FileVersion.Common.ZiCharacter]::FromString($f, $ch - $iconCPOffset, $font, $locationIcon, $txt)
-        $f.AddCharacter($ch, $character)
-      }
-    }
-
-    $file = Get-Item $textFont
-    $filename = $file.basename
-    $f.Name = $filename
-
-    New-Item -ItemType Directory -Path $path -Force | Out-Null
-    $outfile = Join-Path -Path $path -ChildPath ("{0} {1} ({2}).zi" -f $filename, $fontsize, $Codepage)
-    Write-Host "Writing $outfile"
-    $f.Save($outfile)
   }
+
+  $file = Get-Item $textFont
+  $filename = $file.basename
+  $f.Name = $filename
+
+  New-Item -ItemType Directory -Path $path -Force | Out-Null
+  $outfile = Join-Path -Path $path -ChildPath ("{0} {1} ({2}).zi" -f $filename, $textFontSize, $Codepage)
+  Write-Host "Writing $outfile"
+  $f.Save($outfile)
+
 }
 
 Main
